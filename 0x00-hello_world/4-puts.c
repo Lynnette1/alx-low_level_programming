9650121b32cc1c6ab20096c5c9ce09dee9d619cc
Lynnette Asudi <lynnetteasudi@gmail.com>
2023-03-21 01:54:44 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#include <stdio.h>
+
+int main()
+{
+	//string initialisation
+    char putsMystr[] = "Programming is like building a multilingual puzzle";
+    
+    puts(putsMystr); //writing the string
+    
+    return 0;
+}
+    
