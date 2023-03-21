f7028d0eafb5fec5eae203907e0d3003ff9a295a
Lynnette Asudi <lynnetteasudi@gmail.com>
2023-03-21 03:30:56 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
