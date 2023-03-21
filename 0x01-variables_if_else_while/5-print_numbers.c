7d6562319123fb18e9e14e2cf20ef5175ff15bc6
Lynnette Asudi <lynnetteasudi@gmail.com>
2023-03-21 12:21:09 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,12 @@
-#!/bin/bash
-
 #include <stdio.h>
 
-
-int main()
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+ 
+int main(void)
 {
 	int num;
 
